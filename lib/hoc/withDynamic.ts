4e9f3e53afafab6827f92c48b20f81550fa81e31
simba0808 <simba960808@gmail.com>
2024-03-31 07:05:05 +1000

    fix: eslint error in web3client

--- lib/hoc/withDynamic.ts
+++ lib/hoc/withDynamic.ts
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic';
 import type React from 'react';
 
 const withDynamic = <T>(Component: React.FC<T>) => {
-  return dynamic(() => Promise.resolve(Component), { ssr: false });
+  return dynamic(() => Promise.resolve(Component), {ssr: false});
 };
 
 export default withDynamic;
