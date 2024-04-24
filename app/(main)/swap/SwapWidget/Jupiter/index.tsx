7f79231e1d84d4088d5a145d1e8add70dc150ae5
simba <153499114+simba0808@users.noreply.github.com>
2024-04-25 00:38:13 +1000

    fix: balance usdt in swap page

--- app/(main)/swap/SwapWidget/Jupiter/index.tsx
+++ app/(main)/swap/SwapWidget/Jupiter/index.tsx
@@ -1,5 +1,6 @@
-import {useState} from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
 import {useAccount} from 'wagmi';
+import {useWeb3Modal} from '@web3modal/react';
 
 import useAuthSession from '@/lib/auth/hooks/useAuthSession';
 import Selector from '@/lib/components/selector/Selector';
@@ -9,12 +10,27 @@ import BorderContainer from '@/lib/components/BorderContainer';
 
 import tokenList from '../tokenList';
 
+import {Balance} from './Balance';
+
+
 const JupiterForm = () => {
   const {session} = useAuthSession('siwe');
-  const {address: account, isConnected} = useAccount();
+  const {address, isConnected} = useAccount();
 
-  const[inputToken, setInputToken] = useState(tokenList[1]);
-  const [outputToken, setOutputToken] = useState(tokenList[0]);
+  const [selected, setSelected] = useState(0);
+  const [outSelected, setOutSelected] = useState(1);
+
+  const[inputToken, setInputToken] = useState(tokenList[selected]);
+  const [outputToken, setOutputToken] = useState(tokenList[outSelected]);
+
+  const {open} = useWeb3Modal();
+
+  const modalOpened = useRef(false);
+
+  useEffect(() => {
+    setInputToken(tokenList[selected]);
+    setOutputToken(tokenList[outSelected]);
+  }, [selected, outSelected]);
 
   const handleSwitch = () => {
     const temp = inputToken;
@@ -22,40 +38,54 @@ const JupiterForm = () => {
     setOutputToken(temp);
   };
 
+  const onReconnect = useCallback(async () => {
+    if (!address) {
+      // Clear WalletConnect and wagmi related data from localStorage
+      localStorage.removeItem('walletconnect');
+      localStorage.removeItem('wagmi.wallet');
+      localStorage.removeItem('wagmi.store');
+      localStorage.removeItem('wagmi.connected');
+  
+      modalOpened.current = true;
+      await open(); // This should now prompt for connection as the previous session data is cleared
+      return;
+    }
+  }, [address, open]);
+
   return (
     <BorderContainer>
       <div className='py-8 flex flex-col gap-4 bg-dark-blue rounded-2xl'>
         <div className='w-full px-4 sm:px-10'>
-          <h2 className='text-[12px] lg:text-[32px] font-bold uppercase'>RFD Swap</h2>
+          <h2 className='text-[12px] lg:text-[32px] font-bold uppercase'>RFT Swap</h2>
           <div>
             <div className='flex justify-between text-landing-content font-bold'>
               <span>You PAY</span>
-              <span>BALANCE: 0</span>
+              <Balance token={inputToken} />
             </div>
             <div className='flex justify-between items-center w-full shadow-lg px-3 sm:px-8 py-4 my-2 rounded-xl bg-[#05111C]'>
-              <Selector />
+              <Selector selected={selected} setSelect={setSelected} />
               <div className='w-[100px] h-full rounded-xl bg-[#05111C]'>
                 <input type='text' className='w-full h-full inset-0 px-2 text-right text-[12px] lg:text-[24px] font-semibold bg-transparent outline-none' />
               </div>
             </div>
           </div>
-          <div className='my-4 flex justify-center'>
+          <div className='my-2 lg:my-4 flex justify-center'>
             <span className='flex flex-col gap-2 p-2'>
               <span>
-                <img className='w-8' src='/img/ArrowUp.svg' alt='up' />
+                <img className='w-4 lg:w-8' src='/img/ArrowUp.svg' alt='up' />
               </span>
               <span>
-                <img className='w-8' src='/img/ArrowDown.svg' alt='down' />
+                <img className='w-4 lg:w-8' src='/img/ArrowDown.svg' alt='down' />
               </span>
             </span>
           </div>
           <div className='mb-8'>
             <div className='flex justify-between text-landing-content font-bold'>
               <span>You Receive</span>
-              <span>BALANCE: 0</span>
+              <Balance token={outputToken} />
             </div>
             <div className='flex justify-between items-center w-full shadow-lg px-3 sm:px-8 py-4 my-2 rounded-xl bg-[#05111C]'>
-              <Selector />
+              <Selector selected={outSelected} setSelect={setOutSelected} />
               <div className='w-[100px] h-full rounded-xl bg-[#05111C]'>
                 <input type='text' className='w-full h-full inset-0 px-2 text-[12px] lg:text-[24px] font-semibold text-right bg-transparent outline-none' />
               </div>
@@ -64,12 +94,21 @@ const JupiterForm = () => {
           <div className='mt-4'>
             {
               isConnected && session ? (
-                <Button className='px-10 py-3 box-shadow' variant='transparent' size='large'>
+                <Button 
+                  className='box-shadow text-[12px] lg:text-xl px-8 py-3 lg:py-5 lg:px-20 rounded-md lg:rounded-2xl' 
+                  variant='transparent' 
+                  size='large'
+                >
                   Swap
                 </Button>
               ) : (
                 session ?
-                  <Button className='box-shadow' variant='transparent' size='large'>
+                  <Button 
+                    className='box-shadow text-[12px] lg:text-xl px-8 py-3 lg:py-5 lg:px-20 rounded-md lg:rounded-2xl' 
+                    variant='transparent' 
+                    size='large' 
+                    onClick={onReconnect}
+                  >
                     Connect Wallet
                   </Button> :
                   <SiweButton className='box-shadow text-[12px] lg:text-xl px-8 py-3 lg:py-5 lg:px-20 rounded-md lg:rounded-2xl' />
--- app/(main)/swap/UsdtTransfer/index.tsx
+++ app/(main)/swap/UsdtTransfer/index.tsx
@@ -1,28 +1,60 @@
 'use client';
-import {useState} from 'react';
+import {useCallback, useRef, useState} from 'react';
+import {useAccount} from 'wagmi';
+import {useWeb3Modal} from '@web3modal/react';
+import {useBalance} from 'wagmi';
 
+import useAuthSession from '@/lib/auth/hooks/useAuthSession';
 import BorderContainer from '@/lib/components/BorderContainer';
 import Button from '@/lib/components/button/Button';
+import SiweButton from '@/app/signin/SiweButton';
+import tokenList from '@/lib/wallet/TokenList';
 
 const USDTTransferWidget = () => {
+  const {session} = useAuthSession('siwe');
+  const {address, isConnected} = useAccount();
+
   const [transferDirection, setTransferDirection] = useState(true);
+
+  const {open} = useWeb3Modal();
+
+  const modalOpened = useRef(false);
+
+  const {data: usdtOnWallet} = useBalance({
+    address: address,
+    token: tokenList[1]?.tokenAddress,
+  });
+
+  const onReconnect = useCallback(async () => {
+    if (!address) {
+      // Clear WalletConnect and wagmi related data from localStorage
+      localStorage.removeItem('walletconnect');
+      localStorage.removeItem('wagmi.wallet');
+      localStorage.removeItem('wagmi.store');
+      localStorage.removeItem('wagmi.connected');
+  
+      modalOpened.current = true;
+      await open(); // This should now prompt for connection as the previous session data is cleared
+      return;
+    }
+  }, [address, open]);
 
   return (
     <div className='text-white text-center'>
       <h2 className='heading-h2 lg:mb-4'>Usdt transfers</h2>
       <p className="text-normal-content mb-6 lg:mb-10">Type the amount. Select Transfer direction</p>
-      <div className='flex flex-col gap-4'>
+      <div className='flex flex-col gap-2 lg:gap-4'>
         <BorderContainer>
           <div className='py-8 bg-dark-blue rounded-2xl backdrop-blur-2xl'>
             <p className='text-[12px] lg:text-[32px] font-bold uppercase'>USDT on platform</p>
             <p className='text-[18px] lg:text-[64px] font-bold'>$0,000</p>
           </div>
         </BorderContainer>
-        <div className='relative h-10 lg:h-20 flex justify-between'>
-          <div className='basis-1/3 h-full rounded-xl bg-[#05111C]'>
+        <div className='relative h-8 lg:h-20 flex justify-between'>
+          <div className='basis-1/3 h-full rounded-md lg:rounded-xl bg-[#05111C]'>
             <input 
               type='text' 
-              className='w-full h-full inset-0 px-2 text-[12px] lg:text-[36px] text-center font-medium rounded-x bg-transparent outline-none' 
+              className='w-full h-full inset-0 px-2 text-[12px] lg:text-[36px] text-center font-medium bg-transparent outline-none' 
               defaultValue={0}
             />
           </div>
@@ -36,16 +68,37 @@ const USDTTransferWidget = () => {
               </div>
             </div>
           </div>
-          <Button size='large' variant='transparent' className='h-full box-shadow text-[12px] lg:text-xl px-8 py-3 lg:py-4 lg:px-20 box-shadow rounded-md lg:rounded-2xl'>
+          <Button 
+            size='large' 
+            variant='transparent' 
+            className='h-full box-shadow text-[10px] lg:text-xl px-2 py-0 lg:py-4 lg:px-20 box-shadow rounded-md lg:rounded-2xl'
+          >
             {transferDirection?'Deposit USDT':'Withdraw USDT'}
           </Button>
         </div>
         <BorderContainer>
           <div className='py-8 bg-dark-blue rounded-2xl backdrop-blur-2xl'>
             <p className='text-[12px] lg:text-[32px] font-bold mb-4 uppercase'>USDT on wallet</p>
-            <Button className='box-shadow text-[12px] lg:text-xl px-8 py-3 lg:py-5 lg:px-20 rounded-md lg:rounded-2xl' size='large' variant='transparent'>
-              Connect Web3 Wallet
-            </Button>
+            {
+              isConnected && session ? (
+                <p className='text-[18px] lg:text-[64px] font-bold'>
+                  ${Number(usdtOnWallet?.formatted || 0).toLocaleString(undefined, {minimumFractionDigits: 3})}
+                </p>
+              ) : (
+                session ? (
+                  <Button 
+                    className='box-shadow text-[12px] lg:text-xl px-8 py-3 lg:py-5 lg:px-20 rounded-md lg:rounded-2xl' 
+                    size='large' 
+                    variant='transparent'
+                    onClick={onReconnect}
+                  >
+                    Connect Wallet
+                  </Button>
+                ) : (
+                  <SiweButton className='box-shadow text-[12px] lg:text-xl px-8 py-3 lg:py-5 lg:px-20 rounded-md lg:rounded-2xl' />
+                )
+              )
+            }
           </div>
         </BorderContainer>
         <p className='text-normal-content'>Press Transfer button. Confirm web3 transaction</p>
